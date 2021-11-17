1252028da2af122d85b6e02fce52f8508bd81859
legir <arigelh@gmail.com>
2021-11-17 10:44:10 +0800

    Added favorite functionality 2

--- src/EntryDetails.js
+++ src/EntryDetails.js
@@ -13,10 +13,35 @@ const EntryDetails = () => {
             history.push('/');
         })
     }
+    
     const handelClickFavorite= () => {
         fetch('http://localhost:8000/entries/' + entry.id, {
             //set favorite in entry to true put or patch
+            method: 'PATCH',
+            headers: { 'Content-Type':'application/json'},
+            body: JSON.stringify({ favorite: true })
             
+        }).then(() => {
+            history.push('/');
+        })
+    }
+
+    const handelClickFavoriteRemove= () => {
+        fetch('http://localhost:8000/entries/' + entry.id, {
+            //set favorite in entry to true put or patch
+            method: 'PATCH',
+            headers: { 'Content-Type':'application/json'},
+            body: JSON.stringify({ favorite: false })
+            
+        }).then(() => {
+            history.push('/');
+        })
+    }
+    const handelClickAddAsNewEntry= () => {
+        fetch('http://localhost:8000/entries/' + entry.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
         })
     }
 
@@ -32,7 +57,9 @@ const EntryDetails = () => {
                     <div>{ entry.body }</div>
                     
                     <button onClick = {handelClick}>delete</button>
-                    <button onClick = {handelClickFavorite}>Favorite</button>
+                    <button onClick = {handelClickFavorite}>Add Favorite</button>
+                    <button onClick = {handelClickFavoriteRemove}>Remove Favorite</button>
+                    <button onClick = {handelClickAddAsNewEntry}>Add As New Entry</button>
                 </article>
             )}
         </div>
